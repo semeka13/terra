01eccfd16d74d2905058ffaad95bb558cac45055
leffff <levnovitskiy@gmail.com>
2021-04-09 09:55:05 +1000

    Recommender example fixed

--- ml/recommendations/example_recommendation.py
+++ ml/recommendations/example_recommendation.py
@@ -1,11 +1,11 @@
-from ml.recommendations.recommendation_model import CatBoostRecomender
+from ml.recommendations.recommendation_model import CatBoostRecommender
 from sklearn.datasets import load_breast_cancer
 X, y = load_breast_cancer(return_X_y=True)
 
-cbr = CatBoostRecomender()
+cbr = CatBoostRecommender()
 cbr.fit(X[:200], y[:200])
 cbr.save("cb.pickle")
 
-cbr2 = CatBoostRecomender()
+cbr2 = CatBoostRecommender()
 cbr2.load("cb.pickle")
 print(cbr2.predict(X[200:]))
